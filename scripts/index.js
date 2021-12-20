fa0abdfccca6aff2838289251c4df0f116b3fa01
Lakatosska <vizualnoe@gmail.com>
2021-12-21 00:48:35 +0300

    added form functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,28 @@
+// Форма
+
+const formElement = document.querySelector('.form')
+
+const nameInput = formElement.querySelector('.form__input_type_name')
+const emailInput = formElement.querySelector('.form__input_type_email')
+const messageInput = formElement.querySelector('.form__input_type_message')
+
+const applicationButton = formElement.querySelector('.button_place_form')
+
+applicationButton.addEventListener('click', () => {
+  nameInput.value
+  emailInput.value
+  messageInput.value
+})
+
+function formSubmitHandler (evt) {
+  evt.preventDefault()
+}
+
+formElement.addEventListener('submit', formSubmitHandler)
+
+// Слайдер
+
+
 
 
 
